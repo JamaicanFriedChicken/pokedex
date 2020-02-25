a466d88235221e2f07be13fcec9090773cfac3f9
JamaicanFriedChicken <mikey_lue777@hotmail.com>
2020-02-25 16:58:14 -0500

    added spinner GIF and for better UI implementation and representation

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -2,9 +2,12 @@ import React, { Component } from "react";
 
 import styled from "styled-components";
 
+import spinner from "../layout/spinner.gif";
+
 const Sprite = styled.img`
     width: 5em;
     height 5em;
+    display: none;
     `;
 
 export default class PokemonCard extends Component {
@@ -33,11 +36,25 @@ export default class PokemonCard extends Component {
       <div className="col-md-3 col-sm-6 mb-5">
         <div className="card">
           <h5 className="card-header">{this.state.pokemonIndex}</h5>
+          {this.state.imageLoading ? (
+            <img
+              src={spinner}
+              style={{ width: "5em", height: "5em" }}
+              className="card-img-top rounded mx-auto d-block mt-2"
+            />
+          ) : null}
           <Sprite
             className="card-img-top rounded mx-auto mt-2"
             onLoad={() => this.setState({ imageLoading: false })}
             onError={() => this.setState({ toManyRequests: true })}
             src={this.state.imageUrl}
+            style={
+              this.state.toManyRequests
+                ? { display: "none" }
+                : this.state.imageLoading
+                ? null
+                : { display: "block" }
+            }
           />
           {this.state.toManyRequests ? (
             <h6 className="mx-auto">
