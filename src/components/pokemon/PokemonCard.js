27d75ce77424db2fbf5e43cb6a430c498e66608e
JamaicanFriedChicken <mikey_lue777@hotmail.com>
2020-02-25 14:01:58 -0500

    fetched pokemon and their respective index displaying on a card

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,19 +1,46 @@
 import React, { Component } from "react";
 
+import styled from "styled-components";
+
+const Sprite = styled.img`
+    width: 5em;
+    height 5em;
+    `;
+
 export default class PokemonCard extends Component {
   state = {
     name: "",
     imageUrl: "",
     pokemonIndex: ""
   };
-  render() {
-    const { name, url } = this.props;
 
+  componentDidMount() {
+    const { name, url } = this.props;
+    const pokemonIndex = url.split("/")[url.split("/").length - 2];
+    const imageUrl = `https://github.com/PokeAPI/sprits/blob/master/sprites/pokemon/${pokemonIndex}.png`;
+
+    this.setState({
+      name: name,
+      imageUrl: imageUrl,
+      pokemonIndex: pokemonIndex
+    });
+  }
+
+  render() {
     return (
       <div className="col-md-3 col-sm-6 mb-5">
         <div className="card">
-          <div className="card-header">
-            <h1>{name}</h1>
+          <h5 className="card-header">{this.state.pokemonIndex}</h5>
+          <div className="card-body mx-auto">
+            <h6 className="card-title">
+              {this.state.name
+                .toLowerCase()
+                .split(" ")
+                .map(
+                  letter => letter.charAt(0).toUpperCase() + letter.substring(1)
+                )
+                .join("")}
+            </h6>
           </div>
         </div>
       </div>
