66b2517fa00d51e1cf4512a056057e0983e71d89
JamaicanFriedChicken <mikey_lue777@hotmail.com>
2020-02-25 16:41:49 -0500

    added pokemon sprites and necessary loading messages for errors

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -11,13 +11,15 @@ export default class PokemonCard extends Component {
   state = {
     name: "",
     imageUrl: "",
-    pokemonIndex: ""
+    pokemonIndex: "",
+    imageLoading: true,
+    toManyRequests: false
   };
 
   componentDidMount() {
     const { name, url } = this.props;
     const pokemonIndex = url.split("/")[url.split("/").length - 2];
-    const imageUrl = `https://github.com/PokeAPI/sprits/blob/master/sprites/pokemon/${pokemonIndex}.png`;
+    const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
 
     this.setState({
       name: name,
@@ -31,6 +33,19 @@ export default class PokemonCard extends Component {
       <div className="col-md-3 col-sm-6 mb-5">
         <div className="card">
           <h5 className="card-header">{this.state.pokemonIndex}</h5>
+          <Sprite
+            className="card-img-top rounded mx-auto mt-2"
+            onLoad={() => this.setState({ imageLoading: false })}
+            onError={() => this.setState({ toManyRequests: true })}
+            src={this.state.imageUrl}
+          />
+          {this.state.toManyRequests ? (
+            <h6 className="mx-auto">
+              <span className="badge badge-danger mt-2">
+                To Many Requests being made.
+              </span>
+            </h6>
+          ) : null}
           <div className="card-body mx-auto">
             <h6 className="card-title">
               {this.state.name
