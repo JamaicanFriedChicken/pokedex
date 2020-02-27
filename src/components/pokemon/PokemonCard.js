775b846fc73b9b79904fe3b7c498bf489170112b
JamaicanFriedChicken <mikey_lue777@hotmail.com>
2020-02-27 08:38:13 -0500

    added background for web app

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -8,7 +8,25 @@ const Sprite = styled.img`
     width: 5em;
     height 5em;
     display: none;
-    `;
+    -moz-user-select: none;
+    -o-user-select: none;
+    -khtml-user-select: none;
+    -webkit-user-select: none;
+    user-select: none;
+`;
+
+const Card = styled.div`
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  &:hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
+  // prevents users from selecting and dragging the cards in the browser
+  -moz-user-select: none;
+  -website-user-select: none;
+  user-select: none;
+  -o-user-select: none;
+`;
 
 export default class PokemonCard extends Component {
   state = {
@@ -34,7 +52,7 @@ export default class PokemonCard extends Component {
   render() {
     return (
       <div className="col-md-3 col-sm-6 mb-5">
-        <div className="card">
+        <Card className="card">
           <h5 className="card-header">{this.state.pokemonIndex}</h5>
           {this.state.imageLoading ? (
             <img
@@ -74,7 +92,7 @@ export default class PokemonCard extends Component {
                 .join("")}
             </h6>
           </div>
-        </div>
+        </Card>
       </div>
     );
   }
