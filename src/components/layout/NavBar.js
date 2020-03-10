07d4354569bfa0c7db82ad5ed222f966d6b11078
JamaicanFriedChicken <mikey_lue777@hotmail.com>
2020-03-09 23:07:55 -0500

    updated README and installed dependencies to publish on github pages

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
 import logo from "../layout/pokedex.png";
-// import styled from "styled-components";
 
 export default class NavBar extends Component {
   render() {
     return (
       <div>
         <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-          {/* <a className="navbar-brand col-sm-3 col-md-2 mr-0 align-items-center"> */}
           <img src={logo} height="30" width="150" />
-          {/* </a> */}
         </nav>
       </div>
     );
